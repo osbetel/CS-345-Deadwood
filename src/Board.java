c3f2d8cef953a9eafcc31509f9a0fe11501cef25
Unknown <osbetel@gmail.com>
2019-02-06 15:52:28 -0800

    Initial commit
    Skeleton class structure in progress
    -Andrew

---
+++ src/Board.java
@@ -0,0 +1,10 @@
+/*
+ * Created by: Andrew Nguyen
+ * Date: 2019-02-05
+ * Time: 16:34
+ * Deadwood
+ */
+
+public class Board {
+
+}
---
+++ src/Player.java
@@ -0,0 +1,9 @@
+/*
+ * Created by: Andrew Nguyen
+ * Date: 2019-02-05
+ * Time: 16:35
+ * Deadwood
+ */
+
+public class Player {
+}
