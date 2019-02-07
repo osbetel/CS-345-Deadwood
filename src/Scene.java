4b1f20a890b8af3ecf52922cbf68fd5a6476e5ea
Andrew DK Nguyen <osbetel@gmail.com>
2019-02-06 17:10:14 -0800

    Skeleton for Scene class. Consider using a Map (hash or tree) for handling roles per scene

--- src/Scene.java
+++ src/Scene.java
@@ -5,6 +5,54 @@
  * Deadwood
  */
 
+import java.util.Map;
+
 public class Scene {
+    private int shotsRemaining;
+
+    private Map<String, Integer> roles; // We can use it as <Role, payout information>, including bonus payouts
+    //MAP IS NOT A CLASS. It's an interface. Choose HashMap or TreeMap (red-black tree) for actual usage
+
+//    private int[] payouts; // [main role, side role, etc...]
+//    private int[] bonusPay;
+
+    public Scene() {
+        //todo
+    }
+
+    public Scene(Map<String, Integer> roles, int shotsRemaining){
+        //todo
+    }
+
+    public int removeShot() {
+        this.shotsRemaining -= 1;
+        return this.shotsRemaining;
+    }
+
+    public void rehearse(Player actor) {
+
+    }
+
+    private void wrapScene() {
+        try {
+            if (shotsRemaining > 0) {
+                throw new Exception("this scene shouldn't be finished yet!");
+            } else {
+                //todo: finish the scene, assign payouts, etc, destroy scene (or file it away as used)
+            }
+        } catch (Exception ex) {
+            //todo: do something
+        }
+    }
+
+    private void payout() {
+        //todo: for each role filled in this.roles, payout
+    }
+
+    private void payoutBonus() {
+        //todo: should only be used upon wrapping the scene
+    }
+
+
 
 }
