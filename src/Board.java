5ddf9b66f4e3cb7289a8474850ebf7b51673945a
Andrew DK Nguyen <osbetel@gmail.com>
2019-02-06 17:09:36 -0800

    Provided skeleton for Board class. Board has access to some Room class methods. Rooms will manage their own Scenes accordingly. Consider having some kind of central System class or database (or a text file) of all the possible scenes, so we can just pass in a random Scene, mark it as used, and then use that passed-in Scene to construct room scenes.

--- src/Board.java
+++ src/Board.java
@@ -6,5 +6,42 @@
  */
 
 public class Board {
+    public Room[] rooms;
+    private Player[] players;
+    private int day;
+
+    public Board(int numPlayers) {
+        //initial setup
+        this.day = 0;
+        //todo: need to loop and initialize players and rooms (don't forget special rooms)
+        //setupGame(numPlayers);
+    }
+
+    private void setupGame(int numPlayers) {
+        //todo: setupGame() should make players, assign money, rank, credits, etc.
+    }
+
+    private void startGame() {
+        //todo: begin the game at the first player. Is there a better way to handle this?
+    }
+
+    private int nextDay() {
+        this.day += 1;
+        return this.day;
+    }
+
+    private void clearScenes() {
+        //removes all scenes from all rooms
+        for (Room r : this.rooms) {
+            r.clearScene();
+        }
+    }
+
+    private void dealScenes() {
+        //deal out scene cards / objects to each room. Called at start of a new day
+        for (Room r : this.rooms) {
+            //todo: r.newScene... from remaining unused scenes
+        }
+    }
 
 }
