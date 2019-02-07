fd4720f89b9edb14463395c2e95383fc14e54fd0
Andrew DK Nguyen <osbetel@gmail.com>
2019-02-06 17:10:46 -0800

    Skeleton for Room class.

--- src/Room.java
+++ src/Room.java
@@ -7,4 +7,27 @@
 
 public class Room {
 
+    private Scene currentScene;
+    private String roomName;
+
+    public Room() {
+        //todo: there's only 10 rooms in total, so define a text file somewhere for use during setup
+    }
+
+    private void setScene(Scene newScene) {
+        this.currentScene = newScene;
+    }
+
+    public void clearScene() {
+        this.currentScene = null;
+    }
+
+    public Scene getScene() {
+        return this.currentScene;
+    }
+
+    public String getRoomName() {
+        return this.roomName;
+    }
+
 }
