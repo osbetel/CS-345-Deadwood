4280c5e9bd4f4f50392b3468f686f60c32a53d26
Jade <jordanj8@wwu.edu>
2019-02-21 17:47:53 -0800

    Updated cost & credit values

--- src/CastingOffice.java
+++ src/CastingOffice.java
@@ -12,7 +12,8 @@ public class CastingOffice extends Room {
 
     public CastingOffice() {
         super("Casting Office", 0, 0);
-        this.dollarCosts = new int[] {2,3,4,5,6}; //todo: Just put the costs in later
+        this.dollarCosts = new int[] {0, 0, 4, 10, 18, 28, 40}; //0 in the 0th and 1st place in the list so price can correlate with index
+        this.creditCost = new int[] {0, 0, 5, 10, 15, 20, 25};
     }
 
 }
